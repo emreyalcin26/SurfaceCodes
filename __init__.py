a6d7678f7338d28d7bd06a7d4ad86b9b155b5cb0
Amelie Schreiber <thesingularity.research@gmail.com>
2020-06-29 20:37:46 -0700

    refactor
    Add dependencies
    Add raise ValueError to drawing
    reorganize files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from SurfaceCodes.surface_code_graph import SurfaceCodeGraph
-from SurfaceCodes.surface_code_circuit import SurfaceCodeCircuit
-from .utilites import permlist_to_tuple
-__all__ = ["SurfaceCodeGraph", "SurfaceCodeCircuit", "permlist_to_tuple"]
