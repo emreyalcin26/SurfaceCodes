1f3afffb3f278a05e781004acbd050bae355b5a2
Amelie Schreiber <thesingularity.research@gmail.com>
2020-06-29 21:33:57 -0700

    refactor

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .surface_code_graph import SurfaceCodeGraph
 from .cirq_surface_codes import CirqSurfaceCodeCircuit
-from .qiskit_surface_code import SurfaceCodeCircuit
+from .qiskit_surface_code import QiskitSurfaceCodeCircuit
 
 from .utilites import permlist_to_tuple
-__all__ = ["SurfaceCodeGraph", "SurfaceCodeCircuit", "permlist_to_tuple", "CirqSurfaceCodeCircuit"]
+__all__ = ["SurfaceCodeGraph", "QiskitSurfaceCodeCircuit", "permlist_to_tuple", "CirqSurfaceCodeCircuit"]
